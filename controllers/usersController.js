8f453395e408a569c662a6bb414680b5459fd660
avrshay <shaymilo23@gmail.com>
2026-05-17 12:33:25 +0300

    json

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,56 +1,112 @@
-const users = require("../models/usersMockData");
-const { sendSuccess, sendError } = require("../middleware/errorHandler");
-
-function getAll(req, res) {
-  const allUsers = users.getAllUsers();
-  return sendSuccess(res, allUsers);
-}
-
-function getById(req, res) {
-  const id = req.params.id;
-  const user = users.geyUserById(id);
-  if (!user) {
-    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
-  }
-  return sendSuccess(res, user);
-}
-
-function newUser(req, res) {
-  const { firstName, lastName, userRole } = req.body;
-  const id = users.createUser(firstName, lastName, userRole);
-  return sendSuccess(res, { userId: id }, 201);
-}
-
-function updateUser(req, res) {
-  const id = req.params.id;
-  const existing = users.geyUserById(id);
-  if (!existing) {
-    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
-  }
-  const { firstName, lastName, userRole } = req.body;
-  users.updateById(id, firstName, lastName, userRole);
-  return sendSuccess(res, { userId: Number(id) });
-}
-
-function updateDetails(req, res) {
-  const id = req.params.id;
-  const existing = users.geyUserById(id);
-  if (!existing) {
-    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
-  }
-  const { firstName, lastName, email, phone_number } = req.body;
-  users.updateDetailsById(id, firstName, lastName, email, phone_number);
-  return sendSuccess(res, { userId: Number(id) });
-}
-
-function deleteUser(req, res) {
-  const id = req.params.id;
-  const existing = users.geyUserById(id);
-  if (!existing) {
-    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
-  }
-  users.deleteById(id);
-  return sendSuccess(res, { userId: Number(id) });
-}
-
-module.exports = { getAll, getById, newUser, updateUser, deleteUser, updateDetails };
+const users = require("../models/usersMockData");
+
+const { sendSuccess, sendError } = require("../middleware/errorHandler");
+
+
+
+function getAll(req, res) {
+
+  const allUsers = users.getAllUsers();
+
+  return sendSuccess(res, allUsers);
+
+}
+
+
+
+function getById(req, res) {
+
+  const id = req.params.id;
+
+  const user = users.geyUserById(id);
+
+  if (!user) {
+
+    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
+
+  }
+
+  return sendSuccess(res, user);
+
+}
+
+
+
+function newUser(req, res) {
+
+  const { firstName, lastName, userRole } = req.body;
+
+  const id = users.createUser(firstName, lastName, userRole);
+
+  return sendSuccess(res, { userId: id }, 201);
+
+}
+
+
+
+function updateUser(req, res) {
+
+  const id = req.params.id;
+
+  const existing = users.geyUserById(id);
+
+  if (!existing) {
+
+    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
+
+  }
+
+  const { firstName, lastName, userRole } = req.body;
+
+  users.updateById(id, firstName, lastName, userRole);
+
+  return sendSuccess(res, { userId: Number(id) });
+
+}
+
+
+
+function updateDetails(req, res) {
+
+  const id = req.params.id;
+
+  const existing = users.geyUserById(id);
+
+  if (!existing) {
+
+    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
+
+  }
+
+  const { firstName, lastName, email, phone_number } = req.body;
+
+  users.updateDetailsById(id, firstName, lastName, email, phone_number);
+
+  return sendSuccess(res, { userId: Number(id) });
+
+}
+
+
+
+function deleteUser(req, res) {
+
+  const id = req.params.id;
+
+  const existing = users.geyUserById(id);
+
+  if (!existing) {
+
+    return sendError(res, 404, "NOT_FOUND", "user not found", { id: Number(id) });
+
+  }
+
+  users.deleteById(id);
+
+  return sendSuccess(res, { userId: Number(id) });
+
+}
+
+
+
+module.exports = { getAll, getById, newUser, updateUser, deleteUser, updateDetails };
+
